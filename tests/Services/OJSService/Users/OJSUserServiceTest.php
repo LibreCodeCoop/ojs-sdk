4dd3bf76639a7a4fb0b9a5056becc5dd45b68b75
Vitor Mattos <vitor@php.rio>
2021-01-20 11:40:21 -0300

    Fix tests

--- tests/Services/OJSService/Users/OJSUserServiceTest.php
+++ tests/Services/OJSService/Users/OJSUserServiceTest.php
@@ -22,20 +22,27 @@ class OJSUserServiceTest extends TestCase
         $this->assertNotEmpty($loginResponse);
     }
 
-    public function testCreateUserReturnTrue()
+    public function testCreateUpdateUserReturnTrue()
     {
-        $mock = new MockHandler([
-            new Response(200, ['Set-cookie' => ['OJSSID=91a6b30db3337105fefa2bc2c2520421; path=/; domain=nginx-ojs']])
+        $OjsClient = new OJSUserService();
+
+        HookRegistry::clear('userdao::_getuserbyemail');
+        HookRegistry::register('userdao::_getuserbyemail', function($hookName, $args) {
+            $args[2] = new ADORecordSet_empty();
+            return true;
+        });
+        $return = $OjsClient->createUpdateUser([
+            'username' => 'test',
+            'password' => 'test',
+            'email' => 'test@test.coop',
+            'mailingAddress' => 'Street 55',
+            'locales' => ['en_US'],
+            'groups' => [1],// administrator
+            'givenName'     => ['en_US' => 'User'],
+            'familyName'    => ['en_US' => 'Test'],
+            'phone'         => '+123456789',
+            'lattes'        => 'https://lattes.com'
         ]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-        $OjsClient = new OJSUserService($client);
-//         $ojsBasePath = getenv('OJS_BASE_PATH');
-//         $this->_client = new Client([
-//             'base_uri' => $ojsBasePath
-//         ]);
-//         $OjsClient = new OJSUserService($this->_client);
-        $return = $OjsClient->createUser('Fulano', 'Souza', 'fulano@teste.com', 'fulano', '123', '7777777777', 'Rua dos Bobos');
         $this->assertTrue($return);
     }
 }
